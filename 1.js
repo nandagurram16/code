2a7dedd1aa69a05897ee65961e85808602ba006c
nandagurram16 <nandagurram168@gmail.com>
2024-11-15 10:27:08 +0530

    Test

---
+++ 1.js
@@ -0,0 +1,91 @@
+// /// // let a=10
+// // // let b=20
+// // // let c='nanda'
+// // // let d='30'
+// // // let e=true
+// // // let f;
+// // // console.log(a+b)
+// // // console.log(a+c)
+// // // console.log(a+d)
+// // // console.log(a+e)
+// // // console.log(a+f)
+// ------------------------------------------
+// // let a=10
+// // let b=20
+// // let c='nanda'
+// // let d='30'
+// // let e=true
+// // let f;
+// // console.log(a*b)
+// // console.log(a*c)
+// // console.log(a*d)
+// // console.log(a*e)
+// // console.log(a*f)
+// ---------------------------------
+// let ename='nanda'
+// let lname='kumar'
+// let num=10
+// console.log(ename*lname)
+// console.log(ename*num)
+// console.log(2**3)
+// ------------------------------------
+// let a='nanda'
+// let sal=10101
+// console.log(a=='nanda' && sal<3000)//false
+// ----------------------------------------------
+// let a=30
+// let b='30'
+// console.log(a==b)//true
+// console.log(a===b)//false
+// ================================================
+// conditional statements
+// let esal=303030
+// if(esal < 40404){
+//     console.log('im good')
+// }
+// else{
+//     console.log('im bad')
+// }
+// ==============================================
+// terinary operator
+// let a=900000
+// a>40000?console.log('greater'):console.log('lesser')
+// ==============================================================
+// odd or even?
+// let a=99;
+// if(a%2==0){
+//     console.log('even num')
+// }
+// else{
+//     console.log('odd num')
+// }
+// ================================================
+// Type Script
+// let score:Array<String>=['12','34','32'];
+// console.log(score)
+// ================================================
+// given number is 3 digit number or not?
+// let num=1344;
+// if (num >99 && num < 1000){
+//     console.log('3 digit number')
+// }
+// else{
+//     console.log('not a 3 digit number')
+// }
+// ===========================================
+// given nmber is divisible by seven or not
+// let num=1234;
+// if(num%7===0){
+//     console.log('divisible by seven')
+// }
+// else{
+//     console.log('not divisible by seven')
+// }
+// =======================================================
+// if('nadakumar'){
+//     console.log('good boy')
+// }
+// else{
+//     console.log('bad boy')
+// }
+// =======================================
